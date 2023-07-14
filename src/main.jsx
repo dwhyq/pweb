c0be26f0e8aee9725544451c5a26ebf28d58354f
dwhyq <dwhyq@outlook.com>
2023-07-14 12:51:35 +0100

    page reload bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
 );
